d6682ccc68c8f59d11ec00fbaa1b9bb0bf14005b
jatziri2 <jatzirilin@ciencias.unam.mx>
2024-02-22 21:04:50 -0600

    Se agrego la segunda parte de la practica

---
+++ alchemyClasses/peliculas.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String
+from alchemyClasses import db
+
+
+class peliculas(db.Model):
+    __tablename__ ='peliculas'
+    idPelicula = Column(Integer, primary_key=True)
+    nombre = Column(String(200))
+    genero = Column(String(45), nullable=True)
+    duracion = Column(Integer, nullable=True)
+    inventario = Column(Integer)
+    
+    def __init__(self, nombre, genero=None, duracion=None, inventario=1):
+        self.nombre = nombre
+        self.genero = genero
+        self.duracion = duracion
+        self.inventario = inventario
+        
+    def __str__(self):
+        return f'Nombre:{self.nombre}\nGenero:{self.genero}\nDuracion:{self.duracion}\nInventario:{self.inventario}\n'
